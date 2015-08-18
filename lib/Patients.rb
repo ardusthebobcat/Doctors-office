4c7ca9354cd0f0ee7e71395969c0434a08764d91
Jake Glassman & Michael Summers <student@epicodus.com>
2015-08-18 14:01:03 -0700

    add doctor_id method to return doctor_id

--- lib/Patients.rb
+++ lib/Patients.rb
@@ -1,11 +1,33 @@
 class Patient
 
-  attr_reader(:id, :name, :birthday ,:doctor_id)
+  attr_reader(:id, :name, :birthdate ,:doctor_id)
 
   define_method(:initialize) do |attributes|
     @id = attributes.fetch(:id)
     @name = attributes.fetch(:name)
-    @birthday = attributes.fetch(:birthday)
+    @birthdate = attributes.fetch(:birthdate)
     @doctor_id = attributes.fetch(:doctor_id)
+  end
+
+  define_method(:==) do |another_patient|
+    self.name().==(another_patient.name()).&self.id().==(another_patient.id()).&self.birthdate().==(another_patient.birthdate()).&self.doctor_id().==(another_patient.doctor_id())
+  end
+
+  define_singleton_method(:all) do
+    returned_patients= DB.exec("SELECT * FROM patients;")
+    patients = []
+    returned_patients.each() do |patient|
+      name = patient.fetch('name')
+      id = patient.fetch('id').to_i()
+      birthdate = patient.fetch('birthdate')
+      doctor_id = patient.fetch('doctor_id').to_i()
+      patients.push(Patient.new({:name => name, :id => id, :birthdate => birthdate, :doctor_id => doctor_id}))
+    end
+    patients
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO patients (name, birthdate, doctor_id) VALUES ('#{@name}', '#{@birthdate}', #{@doctor_id}) RETURNING id;")
+    @id = result.first().fetch('id').to_i()
   end
 end
--- spec/Patients_spec.rb
+++ spec/Patients_spec.rb
@@ -4,8 +4,33 @@ require('Patients')
 describe(Patient) do
   describe('#name') do
     it('returns the name of the patient') do
-      test_patient = Patient.new({:id => nil, :name => "Bugs Bunny", :birthday=>'1980-02-03', :doctor_id => nil})
+      test_patient = Patient.new({:id => nil, :name => "Bugs Bunny", :birthdate=>'1980-02-03', :doctor_id => nil})
       expect(test_patient.name).to eq("Bugs Bunny")
+    end
+  end
+  describe('#==') do
+    it('is the same patient if it has the same properties') do
+      patient1 = Patient.new({:id => nil, :name => "Bugs Bunny", :birthdate=>'1980-02-03', :doctor_id => nil})
+      patient2 = Patient.new({:id => nil, :name => "Bugs Bunny", :birthdate=>'1980-02-03', :doctor_id => nil})
+      expect(patient1).to eq(patient2)
+    end
+  end
+  describe('.all') do
+    it('Returns empty array currently') do
+      expect(Patient.all()).to eq([])
+    end
+  end
+  describe('#save') do
+    it('adds a patient to the patient database') do
+      patient1 = Patient.new({:id => nil, :name => "Bugs Bunny", :birthdate=>'1980-02-03 00:00:00', :doctor_id => 1})
+      patient1.save()
+      expect(Patient.all()).to eq([patient1])
+    end
+  end
+  describe('#doctor_id') do
+    it('returns doctor_id') do
+      patient1 = Patient.new({:id => nil, :name => "Bugs Bunny", :birthdate=>'1980-02-03', :doctor_id => 1})
+      expect(patient1.doctor_id).to eq(1)
     end
   end
 end
